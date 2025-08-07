9b3d7bbdcf568c92d41f02cb8ead3dbacdaf4eaf
shwetanshu1411 <2228149@kiit.ac.in>
2025-08-07 23:34:44 +0530

    small change

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -8,8 +8,11 @@ export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <h1>Hello World {count}</h1>
+
+      <h1>Hyy World {count}</h1>
       <TestComp abc={setCount}/>
       <Button onClick={()=>{
+        setCount((prev)=>prev+1);
         setCount((prev)=>prev+1);
         setCount((prev)=>prev+1);
         console.log(count);}}>Click Me</Button>
